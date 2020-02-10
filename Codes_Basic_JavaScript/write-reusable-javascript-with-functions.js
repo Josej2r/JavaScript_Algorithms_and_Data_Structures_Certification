9c421109ece8a93d8e8554541ad41a87e34f5339
josej2r <josej2r@gmail.com>
2020-02-10 17:50:09 +0100

    42

---
+++ Codes_Basic_JavaScript/write-reusable-javascript-with-functions.js
@@ -0,0 +1,14 @@
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+
+function reusableFunction (){
+  console.log("Hi World");
+}
+
+reusableFunction();
