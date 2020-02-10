d5eecdb98439adae1198485a904998e2552b789b
josej2r <josej2r@gmail.com>
2020-02-10 18:00:27 +0100

    43

---
+++ Codes_Basic_JavaScript/passing-values-to-functions-with-arguments.js
@@ -0,0 +1,13 @@
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs( parm1 , parm2) {
+  console.log(parm1 + parm2);
+
+}
+functionWithArgs(7 , 5);
