7dcc4f1aa4de084cd566efa82a7211d7c9fae20a
josej2r <josej2r@gmail.com>
2020-02-10 18:26:35 +0100

    46

---
+++ Codes_Basic_JavaScript/return-a-value-from-a-function-with-return.js
@@ -0,0 +1,13 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive (arg){
+return arg * 5;
+}
+
+console.log(timesFive(5));
+
+console.log(minusSeven(10));
