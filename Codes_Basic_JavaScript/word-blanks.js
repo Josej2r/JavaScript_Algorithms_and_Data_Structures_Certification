7e7aaec20ae64bd117e0c881e89ecd050f05aec4
josej2r <josej2r@gmail.com>
2020-02-10 13:00:47 +0100

    32

---
+++ Codes_Basic_JavaScript/word-blanks.js
@@ -0,0 +1,6 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "There is  a " + myNoun + " that  is " + myAdjective + " and " + myVerb + " and " + myAdverb; // Only change this line;
