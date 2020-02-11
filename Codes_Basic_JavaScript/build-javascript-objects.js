66dd103900bab785e8edff9ad879fdd20c181daa
josej2r <josej2r@gmail.com>
2020-02-11 20:57:31 +0100

    73

---
+++ Codes_Basic_JavaScript/build-javascript-objects.js
@@ -0,0 +1,19 @@
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+
+name: "Margarito",
+legs: 4,
+tails: 1,
+friends :[ 1 , "perro"]
+
+
+};
