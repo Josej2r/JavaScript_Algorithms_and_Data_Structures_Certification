e3c49a481c7c050723e228bbea60d955d75e39ab
josej2r <josej2r@gmail.com>
2020-02-11 12:40:08 +0100

    55

---
+++ Codes_Basic_JavaScript/comparison-with-the-strict-inequality-operator.js
@@ -0,0 +1,10 @@
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
