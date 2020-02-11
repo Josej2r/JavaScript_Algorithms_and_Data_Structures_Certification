f8d4641ce893db04a60dfffa5458598c88725d7f
josej2r <josej2r@gmail.com>
2020-02-11 20:34:26 +0100

    72

---
+++ Codes_Basic_JavaScript/counting-cards.js
@@ -0,0 +1,40 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+switch (card){
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count +=1;
+  break;
+
+  case 7:
+  case 8:
+  case 9:
+  count +=0;
+  break;
+
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+  count -=1;
+  break;
+}
+ if( count >=1){
+   return ( count + " Bet");
+ }
+else{
+  return (count + " Hold");
+}
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(3); cc(3); cc(7); cc('K'); cc('A');
