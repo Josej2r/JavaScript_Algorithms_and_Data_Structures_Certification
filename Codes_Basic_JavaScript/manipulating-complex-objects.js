0517bb4797553ecf63b19246e3728c24b567df37
josej2r <josej2r@gmail.com>
2020-02-12 14:54:49 +0100

    81

---
+++ Codes_Basic_JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+    {
+    "artist": "Michael Jackson",
+    "title": "Thireller",
+    "release_year": 1984,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
