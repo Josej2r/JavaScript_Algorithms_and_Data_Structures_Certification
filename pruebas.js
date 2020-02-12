23a5fea2082dbff588e086d23ac479aeb442458b
josej2r <josej2r@gmail.com>
2020-02-12 12:09:58 +0100

    79

---
+++ pruebas.js
@@ -0,0 +1,25 @@
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams";
+
+     "bravo":"Boston";
+
+     "charlie": "Chicago";
+
+     "delta": "Denver";
+
+     "echo": "Easy";
+
+     "foxtrot": "Frank";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
