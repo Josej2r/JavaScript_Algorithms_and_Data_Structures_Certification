ef53311b1caadf04f437a8ad779b92a847db7667
josej2r <josej2r@gmail.com>
2020-02-13 20:26:27 +0100

    99

---
+++ Codes_Basic_JavaScript/use-recursion-to-create-a-countdown.js
@@ -0,0 +1,13 @@
+//Only change code below this line
+function countdown(n){
+  if( n < 1){
+    return [];
+
+  }else{
+    const countArray = countdown ( n-1);
+        countArray.unshift(n); 
+    return countArray;
+  }
+  return;
+}
+console.log(countdown(5)); // [5, 4, 3, 2, 1]
