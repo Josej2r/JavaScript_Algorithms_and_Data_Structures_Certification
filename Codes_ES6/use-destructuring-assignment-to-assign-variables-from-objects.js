d0c63c516439de7889758a1b3f8e9f80430a4008
josej2r <josej2r@gmail.com>
2020-02-14 21:37:51 +0100

    39

---
+++ Codes_ES6/use-destructuring-assignment-to-assign-variables-from-objects.js
@@ -0,0 +1,15 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// change code below this line
+  
+const{ today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// change code above this line
+
+console.log(yesterday) // should be not defined
+console.log(highToday); // should be 77
+console.log(highTomorrow); // should be 80
