068f630bb99faca759214267cefc0924dd6e53d6
josej2r <josej2r@gmail.com>
2020-02-15 13:38:24 +0100

    61

---
+++ Codes_ES6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,10 @@
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
