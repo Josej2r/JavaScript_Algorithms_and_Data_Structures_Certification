fef50c268b9fc8647a1758d4726666cd194f0014
josej2r <josej2r@gmail.com>
2020-02-15 13:49:21 +0100

    65

---
+++ Codes_ES6/use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,10 @@
+/* Alter code below this line */
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+/* Alter code above this line */
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
