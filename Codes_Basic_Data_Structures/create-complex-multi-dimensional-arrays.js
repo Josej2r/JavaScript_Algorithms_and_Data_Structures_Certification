091684d62064c3c46da2904fd569f629585f4b8b
josej2r <josej2r@gmail.com>
2020-02-19 16:54:01 +0100

    60

---
+++ Codes_Basic_Data_Structures/create-complex-multi-dimensional-arrays.js
@@ -0,0 +1,5 @@
+let myNestedArray = [
+  // change code below this line
+'first_level',['second_level', 'segundo', [ 'deep', 'tercero', ['deeper',['deepest','quinto']]]]
+  // change code above this line
+];
