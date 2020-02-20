87824017bef5542b983d37ed4b6c68f57a28a9ce
josej2r <josej2r@gmail.com>
2020-02-20 21:39:56 +0100

    31

---
+++ Codes_Basic_Algorithm_Scripting/return-largest-numbers-in-arrays.js
@@ -0,0 +1,19 @@
+function largestOfFour(arr) {
+  // You can do this!
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
+}
+
+
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]])
