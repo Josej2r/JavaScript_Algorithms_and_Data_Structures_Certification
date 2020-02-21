92366afc7f336b32460babecea52f0be7cd3913c
josej2r <josej2r@gmail.com>
2020-02-21 14:29:34 +0100

    31

---
+++ Codes_OO_Programming/verify-an-objects-constructor-with-instanceof.js
@@ -0,0 +1,11 @@
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+// Add your code below this line
+
+let myHouse = new House(7);
+
+myHouse instanceof House;
