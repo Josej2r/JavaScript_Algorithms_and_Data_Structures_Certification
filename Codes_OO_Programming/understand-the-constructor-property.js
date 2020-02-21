daccb206be786e5a16d0c0da8a7849e45919000c
josej2r <josej2r@gmail.com>
2020-02-21 19:05:23 +0100

    46

---
+++ Codes_OO_Programming/understand-the-constructor-property.js
@@ -0,0 +1,14 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Add your code below this line
+function joinDogFraternity(candidate) {
+
+  if(candidate.constructor === Dog ){
+    return true;
+  } else {
+    return false;
+  }
+
+}
