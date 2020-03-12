a4878304494425930907f783074777599649d366
José Reyes Romero <36158389+Josej2r@users.noreply.github.com>
2020-03-12 15:58:31 +0100

    Update return-largest-numbers-in-arrays.js

--- Codes_Basic_Algorithm_Scripting/return-largest-numbers-in-arrays.js
+++ Codes_Basic_Algorithm_Scripting/return-largest-numbers-in-arrays.js
@@ -16,4 +16,28 @@ function largestOfFour(arr) {
 }
 
 
+/* Otra posible solución, creación personal
+
+function largestOfFour(arr) {
+  var max;
+  var resultArray=[];
+
+  for(let i=0; i < arr.length;i++){
+    max=arr[i][0];
+    
+    for(let j=0; j < arr[i].length;j++){
+      if(max < arr[i][j]){
+        //console.log(max)
+        max=arr[i][j];
+
+      }
+    }
+  
+  resultArray.push(max);
+  }
+  return resultArray;
+}
+*/
+
+
 largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]])
