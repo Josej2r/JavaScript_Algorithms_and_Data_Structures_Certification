188b78fe8c2e92a41f4c54a78f203557facf2521
josej2r <josej2r@gmail.com>
2020-03-21 14:29:21 +0100

    poco a poco

---
+++ Codes_Functional_Programming/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,12 @@
+// The global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Only change code below this line
+
+return fixedValue+1;
+  // Only change code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
