45132a68a2c944bf6f5ea25f69f0ca2562002dac
josej2r <josej2r@gmail.com>
2020-03-23 13:01:15 +0100

    29

---
+++ Codes_Functional_Programming/return-part-of-an-array-using-the-slice-method.js
@@ -0,0 +1,8 @@
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+var newArr = anim.slice(beginSlice, endSlice)
+return newArr;
+  // Only change code above this line
+}
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
