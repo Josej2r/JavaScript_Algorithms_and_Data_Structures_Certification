4219163edb8bd6ed234dab2f019a5da24cf92273
josej2r <josej2r@gmail.com>
2020-03-23 13:12:03 +0100

    33

---
+++ Codes_Functional_Programming/remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -0,0 +1,9 @@
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  var newArr = cities.slice(0,3);
+  return newArr;
+
+  // Only change code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
