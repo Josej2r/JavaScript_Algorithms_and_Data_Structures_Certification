0f44c6b2716c00d91a546567493ad891b6625572
josej2r <josej2r@gmail.com>
2020-03-25 21:24:49 +0100

    hasta 75

---
+++ Codes_Functional_Programming/combine-an-array-into-a-string-using-the-join-method.js
@@ -0,0 +1,8 @@
+function sentensify(str) {
+  // Only change code below this line
+
+  console.log( str.split(/\W/).join(" "));
+ return str.split(/\W/).join(" ")
+  // Only change code above this line
+}
+sentensify("The.force.is.strong.with.this.one");
---
+++ Codes_Functional_Programming/split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,12 @@
+function splitify(str) {
+  // Only change code below this line
+
+var words=str.split(/\W/);
+
+console.log(str.split(/\W/));
+return words
+
+
+  // Only change code above this line
+}
+splitify("Hello World,I-am code");
