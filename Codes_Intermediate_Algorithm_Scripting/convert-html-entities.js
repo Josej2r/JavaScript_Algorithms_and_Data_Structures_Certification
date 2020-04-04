c4ee5a62a9e452a943944290856722c225202276
josej2r <josej2r@gmail.com>
2020-04-04 14:17:06 +0200

    52

---
+++ Codes_Intermediate_Algorithm_Scripting/convert-html-entities.js
@@ -0,0 +1,30 @@
+function convertHTML(str) {
+  var temp = str.split("");
+
+ 
+
+  for (var i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join("");
+  return temp;
+}
+
+convertHTML("Dolce & Gabbana");
